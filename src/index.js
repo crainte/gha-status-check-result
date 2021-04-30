951b02ee7d0f2a1b29dcd1e68bc3d3c7716f4aa1
Chris Alexander <crainte@hybridoptix.net>
2021-04-30 12:25:36 -0500

    Switch request library, try to handle more types

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-const core = require('@actions/core');
-const axios = require('axios');
-const github = require('@actions/github');
+import { Octokit } from "@octokit/core";
+import { core } from "@actions/core";
+import { github } from "@actions/github";
 
 const token = core.getInput('authToken') || process.env.GITHUB_TOKEN;
 const timeout = core.getInput('timeout') || 30000;
-const interval = core.getInput('interval') || 10000;
+const interval = core.getInput('interval') || 5000;
+const context = core.getInput('context') || null;
 
+const octokit = new Octokit({ auth: token });
 const selfName = process.env.GITHUB_ACTION;
 const selfRepo = process.env.GITHUB_REPOSITORY;
 const selfSha  = process.env.GITHUB_SHA;
-
-const apiUrl = `https://api.github.com/repos/${selfRepo}`;
-const checkUrl = `${apiUrl}/commits/${selfSha}/check-runs`;
-const reqHeaders = {
-    Accept: "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json",
-    Authorization: `token ${token}`
-}
 
 function monitorStatus() {
     console.log("Monitoring for checks and status changes");
@@ -40,8 +35,11 @@ function monitorStatus() {
 
 async function reqChecks() {
     try {
-        const response = await axios.get(checkUrl, { headers: reqHeaders});
-        const filtered = response.check_runs.filter( run => run.name !== selfName );
+        const response = await octokit.request("GET https://api.github.com/repos/{repo}/commits/{sha}/check-runs", {
+            repo: selfRepo,
+            sha: selfSha,
+        });
+        const filtered = response.data.check_runs.filter( run => run.name !== selfName );
         console.log(filtered);
         const failed = filtered.filter(
             run => run.status === "completed" && run.conclusion === "failure"
@@ -53,16 +51,26 @@ async function reqChecks() {
         if (pending.length) return "IN_PROGRESS";
     } catch (error) {
         console.log(error);
-        ret = "FAILURE";
+        return "FAILURE";
     }
     return "SUCCESS";
 }
 
 async function reqStatus() {
     try {
-        const response = await axios.get(checkUrl, { headers: reqHeaders});
-        console.log(response);
-        return response;
+        var filtered;
+        const response = await octokit.request("GET https://api.github.com/repos/{repo}/commits/{sha}/status", {
+            repo: selfRepo,
+            sha: selfSha,
+        });
+        if (context) {
+            // look for the specific context
+            filtered = response.data;
+        } else {
+            filtered = response.data;
+        }
+        console.log(filtered);
+        return filtered;
     } catch (error) {
         console.log(error);
     }
